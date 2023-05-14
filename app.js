46673e60e36e44155f5a136a1d6b2cde0ecb3ae9
Harshit <me.harshitjoshi@gmail.com>
2023-05-15 01:27:09 +0530

    overlay fix modify js

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ $("button#menu").click(function () {
 });
 $("#navbarDropdown").click(function () {
     $(this).toggleClass("is-active");
+    // $("button#menu").toggleClass("is-active");
+});
+$(".dropdown-item").click(function () {
+    $("button#menu").toggleClass("is-active");
+    $('body').toggleClass("overlay");
 });
 
 var header = $('header');
 
-$(window).scroll(function() {
+$(window).scroll(function () {
     var scroll = $(window).scrollTop();
     if (scroll >= 100) {
         $('header').addClass("fixed-top shadow");
@@ -64,38 +69,36 @@ const bestSellingSlider = new Swiper(".best-selling-slider", {
 
 // SMTP Form js
 function sendEmail() {
-  var userName = document.getElementById("name").value;
-  var userEmail = document.getElementById("email").value;
-  var userMobile = document.getElementById("phone").value;
-  var userMessage = document.getElementById("message").value;
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "shantrishadesigns@gmail.com",
-    Password: "21BB890ACD816A38E38177D677E7ED90EFB6",
+    var userName = document.getElementById("name").value;
+    var userEmail = document.getElementById("email").value;
+    var userMobile = document.getElementById("phone").value;
+    var userMessage = document.getElementById("message").value;
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "shantrishadesigns@gmail.com",
+        Password: "21BB890ACD816A38E38177D677E7ED90EFB6",
 
-    To: "shantrishadesigns@gmail.com",
-    From: "shantrishadesigns@gmail.com",
-    Subject: "New Contact form enquiry",
-    ReplyFrom: document.getElementById("email").value,
-    Body:
-      `Hello Admin! Please find below the required information:` +
-      `<br />` +
-      `<br />` +
-      `Name : ` +
-      userName +
-      `<br />` +
-      `Email-Id : ` +
-      userEmail +
-      `<br />` +
-      `Mobile-Number : ` +
-      userMobile +
-      `<br />` +
-      `Message : ` +
-      userMessage
-  }).then((message) =>
-    alert(
-      "Thanks for Submitting. Your details are with us and we will shortly connect with you!"
-    )
-  );
+        To: "shantrishadesigns@gmail.com",
+        From: "shantrishadesigns@gmail.com",
+        Subject: "New Contact form enquiry",
+        ReplyFrom: document.getElementById("email").value,
+        Body: `Hello Admin! Please find below the required information:` +
+            `<br />` +
+            `<br />` +
+            `Name : ` +
+            userName +
+            `<br />` +
+            `Email-Id : ` +
+            userEmail +
+            `<br />` +
+            `Mobile-Number : ` +
+            userMobile +
+            `<br />` +
+            `Message : ` +
+            userMessage
+    }).then((message) =>
+        alert(
+            "Thanks for Submitting. Your details are with us and we will shortly connect with you!"
+        )
+    );
 }
-
